2c5896cabb19fdd7422ced36b21c878fbad3b7f1
Oğuzhan Karakuş <oguzhan.karakus@mobillium.com>
2021-03-11 16:22:00 +0300

    first commit

---
+++ programmatically-ui/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  programmatically-ui
+//
+//  Created by Oğuzhan Karakuş on 11.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
