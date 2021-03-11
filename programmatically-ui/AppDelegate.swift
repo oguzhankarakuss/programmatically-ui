0d85e4ddd489fd67305a069e5ced4168b228edca
Oğuzhan Karakuş <oguzhan.karakus@mobillium.com>
2021-03-11 16:39:26 +0300

    Add rootController in scene and app delegate

--- programmatically-ui/AppDelegate.swift
+++ programmatically-ui/AppDelegate.swift
@@ -10,10 +10,13 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+   var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let viewController = ViewController()
+        self.window?.rootViewController = viewController
+        self.window?.makeKeyAndVisible()
         return true
     }
 
